cf874046fb7368049c3ae99200a1bc2d4d98f6b6
sokainakanwal <25582@students.riphah.edu.pk>
2023-06-08 08:33:32 -0700

    Init

---
+++ app/src/main/java/com/example/easyfood/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.easyfood.util
+
+class Constants{
+    companion object{
+        const val CATEGORY_NAME:String="cat_name"
+        const val MEAL_AREA:String="Meal_area"
+        const val MEAL_ID:String="Mead_ID"
+        const val MEAL_NAME:String="Meal_Name"
+        const val MEAL_STR:String="Meal_str"
+        const val MEAL_THUMB:String="Meal_thumb"
+        const val MEAL_THUMBmSheetDialogFragment:String="MEAL_THUMBmSheetDialogFragment"
+    }
+}
